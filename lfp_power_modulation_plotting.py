833b6c0f5d3625cf68b9276240ded00d96b2034b
chebrol <Blissinbeing2020#>
2021-02-26 12:40:22 +0100

    imported needed libraries

---
+++ lfp_power_modulation_plotting.py
@@ -0,0 +1,9 @@
+from os.path import join
+import numpy as np
+import matplotlib.pyplot as plt
+import quantities as pq
+import neo
+from neo import utils
+from elephant.signal_processing import wavelet_transform
+import statistics
+
